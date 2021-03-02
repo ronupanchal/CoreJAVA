c7be927dd8598dcae5ffa4456880702695f2cd03
Ronak Panchal <ronubca@gmail.com>
2021-03-02 09:56:24 +0530

    Add JAVA PPT and sticker applet program

---
+++ javaapplet/TestApplet.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.applet.*;
+/*
+<applet code="TestApplet" width="300" height="400">
+</applet>
+*/
+public class TestApplet extends Applet
+{
+   public void paint(Graphics g)
+   {
+        g.drawString("This is my applet" ,20,20);
+   }
+}
